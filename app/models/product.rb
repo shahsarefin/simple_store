fd3f030af04db1c6d019573bebe08cdd2c9b07b7
shahsarefin <shahsarefin@gmail.com>
2024-02-25 18:31:05 -0600

    Added foreign key reference from Product to Category and set model associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,7 @@ class Product < ApplicationRecord
     validates :title, presence: true
     validates :price, presence: true
     validates :stock_quantity, presence: true
+
+    belongs_to :category
   end
-  
+
