568674da7efa227e81469c6fd34e56ddbad392d1
shahsarefin <shahsarefin@gmail.com>
2024-02-25 18:35:06 -0600

    Added products.csv and updated seeds script to import product and category data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-676.times do
-    Product.create(
-        title: Faker::Commerce.product_name,
-        price: Faker::Commerce.price(range: 0..100.0),
-        stock_quantity: Faker::Number.between(from: 0, to: 100)
-    )
+# 676.times do
+#     Product.create(
+#         title: Faker::Commerce.product_name,
+#         price: Faker::Commerce.price(range: 0..100.0),
+#         stock_quantity: Faker::Number.between(from: 0, to: 100)
+#     )
+# end
+
+require "csv"
+
+# Clear existing data to prevent issues with duplicate entries
+Product.destroy_all
+Category.destroy_all
+
+csv_file_path = Rails.root.join('db', 'products.csv')
+
+CSV.foreach(csv_file_path, headers: true) do |row|
+  category_name = row['Category']
+  category = Category.find_or_create_by!(name: category_name)
+  
+  category.products.create!(
+    title: row['Title'],
+    price: row['Price'],
+    stock_quantity: row['Stock Quantity']
+  )
 end
+
+puts "Imported #{Product.count} products."
+
+
