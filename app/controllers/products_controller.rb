d9e6520f19869f21109e433625f225cbfa4aba19
shahsarefin <shahsarefin@gmail.com>
2024-02-25 19:26:10 -0600

    products controller updated

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,12 @@
 class ProductsController < ApplicationController
+
   def index
-    @products = Product.includes(:category).all #loading all products from the Product model
+    @products = Product.includes(:category).all
   end
+  
 
   def show
     @product = Product.find(params[:id]) #loading a single product from the Product model
+  
   end
 end
