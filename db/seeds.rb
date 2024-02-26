7f801bae0385f6a5b92ef568602e255cbf0bd450
shahsarefin <shahsarefin@gmail.com>
2024-02-25 19:25:06 -0600

    db row name issue solved

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,16 +15,25 @@ Category.destroy_all
 csv_file_path = Rails.root.join('db', 'products.csv')
 
 CSV.foreach(csv_file_path, headers: true) do |row|
-  category_name = row['Category']
-  category = Category.find_or_create_by!(name: category_name)
+    category_name = row['category'].try(:strip)
+    title = row['name'].try(:strip) 
+    price = row['price'].try(:strip)
+    stock_quantity = row['stock quantity'].try(:strip) 
   
-  category.products.create!(
-    title: row['Title'],
-    price: row['Price'],
-    stock_quantity: row['Stock Quantity']
-  )
-end
+    if category_name.present? && title.present? && price.present? && stock_quantity.present?
+      category = Category.find_or_create_by!(name: category_name)
+      category.products.create!(
+        title: title,
+        price: price,
+        stock_quantity: stock_quantity
+      )
+    else
+      puts "Skipping invalid row: #{row.inspect}"
+    end
+  end
+  
 
 puts "Imported #{Product.count} products."
+
 
 
